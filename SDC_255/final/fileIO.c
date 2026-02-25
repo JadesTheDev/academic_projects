dbaee2d1472640d4d6685cd0655aaf83357d8aef
JadesTheDev <jades.the.dev@gmail.com>
2026-02-24 22:07:18 -0500

    Create fileIO.c

---
+++ SDC_255/final/fileIO.c
@@ -0,0 +1,51 @@
+// fileIO.c
+// Implements readFile() and writeFile()
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "fileIO.h"
+
+char* readFile(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) return NULL;
+
+    char *buffer = (char*)malloc(1);
+    if (buffer == NULL) {
+        fclose(file);
+        return NULL;
+    }
+
+    int ch;
+    int size = 0;
+
+    while ((ch = fgetc(file)) != EOF)
+    {
+        char *temp = (char*)realloc(buffer, size + 2);
+        if (temp == NULL) {
+            free(buffer);
+            fclose(file);
+            return NULL;
+        }
+        buffer = temp;
+        buffer[size++] = (char)ch;
+    }
+
+    buffer[size] = '\0';
+    fclose(file);
+    return buffer;
+}
+
+void writeFile(const char *filename)
+{
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    fputs("FileIO\n", file);
+    fputs("This file was written by writeFile().\n", file);
+
+    fclose(file);
+}
