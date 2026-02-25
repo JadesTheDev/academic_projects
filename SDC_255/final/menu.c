3bad9ac9bc72a392876a6a1269af92ce9fa900b5
JadesTheDev <jades.the.dev@gmail.com>
2026-02-24 22:08:17 -0500

    Create menu.c

---
+++ SDC_255/final/menu.c
@@ -0,0 +1,34 @@
+// menu.c
+// Menu implementation
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "menu.h"
+
+int menu(void)
+{
+    int choice = 0;
+
+    // Clear the screen (portable)
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+
+    // Display menu options
+    printf("==== Main Menu ====\n");
+    printf("1. First Calculation\n");
+    printf("2. Second Calculation\n");
+    printf("3. Write to File\n");
+    printf("4. Read from File\n");
+    printf("5. Exit\n");
+    printf("===================\n");
+    printf("Select an option: ");
+
+    // Get user input
+    scanf("%d", &choice);
+
+    return choice;
+}
