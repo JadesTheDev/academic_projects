c0808ccd4ce82644ca29defa8e469c7d127c22e1
Jade Powell <jades.the.dev@gmail.com>
2026-08-04 09:40:42 -0400

    Update menu.jsx

--- sdc445/from_the_farm/components/menu.jsx
+++ sdc445/from_the_farm/components/menu.jsx
@@ -1,1 +1,22 @@
+import React from "react";
 
+function Menu() {
+  return (
+    <div className="menu">
+      <h2>From the Farm</h2>
+
+      <ul>
+        <li>🏠 Home</li>
+        <li>🧺 Products</li>
+        <li>🚜 Suppliers</li>
+        <li>🗺 Map</li>
+        <li>📅 Seasonal Calendar</li>
+        <li>📰 News</li>
+        <li>❤️ Favorites</li>
+        <li>🛒 Cart</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
