57ee73d9ee95c0b0065d63b8dc418fefe9a261ee
Jade Powell <jades.the.dev@gmail.com>
2026-08-04 11:48:35 -0400

    Refactor imports in index.js for better structure

--- sdc445/from_the_farm/src/index.js
+++ sdc445/from_the_farm/src/index.js
@@ -1,8 +1,8 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../pages/homepage/index.css";
-import App from "../pages/homepage/App";
+
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
