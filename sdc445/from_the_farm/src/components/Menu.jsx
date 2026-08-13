a78d218b6ad0313ed713bb5eed22c9c57bd65c55
Jade Powell <jades.the.dev@gmail.com>
2026-08-13 00:12:21 -0400

    Refactor Menu component to add new click handlers

--- sdc445/from_the_farm/src/components/Menu.jsx
+++ sdc445/from_the_farm/src/components/Menu.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 
-function Menu({ onProfileClick }) {
+function Menu({
+  onHomeClick,
+  onMapClick,
+  onCalendarClick,
+  onLoginClick,
+  onProfileClick
+}) {
   return (
     <div className="menu">
       <h2>From the Farm</h2>
 
       <ul>
-        <li>🏠 Home</li>
+        <li onClick={onHomeClick}>🏠 Home</li>
         <li>🧺 Products</li>
         <li>🚜 Suppliers</li>
-        <li>🗺 Map</li>
-        <li>📅 Seasonal Calendar</li>
+        <li onClick={onMapClick}>🗺 Map</li>
+        <li onClick={onCalendarClick}>📅 Seasonal Calendar</li>
         <li>📰 News</li>
+        <li onClick={onLoginClick}>🔐 Log In</li>
         <li onClick={onProfileClick}>👤 Profile</li>
       </ul>
     </div>
